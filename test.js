7ebf733ce7af78892b1402b3e2605eb583936745
Anton Shnip <toni.mr.anton@mail.ru>
2022-08-05 11:44:17 +0300

    feature: change tx sender

--- test.js
+++ test.js
@@ -11,16 +11,16 @@ let text = Math.random().toString() + 'lorem ipsum ksjdvnowjsdcdfssdfwrvnolwrnv'
 document.text(text);
 document.end();
 
-const wallet = new Wallet('http://206.189.21.154:3001/v1');
+const wallet = new Wallet('http://159.189.112.134:3001/v1');
 const tx = new Transaction('data');
 
-const sender = '02941c359bd9dc27990ef8bceb1466b5b832bad4289dae9ed77facf5850559790e';
+const sender = '03c8348aad4f8f0241423cfab24bfb44346227c599901da6ee3b50b3abdab4bd15';
 // privateKey: b01f79c616f252130fd877ab692be3ba9432b383eeb30c3c73546581e4ff4aa0 for 029
 
-const recipient = '03c8348aad4f8f0241423cfab24bfb44346227c599901da6ee3b50b3abdab4bd15';
+const recipient = '02941c359bd9dc27990ef8bceb1466b5b832bad4289dae9ed77facf5850559790e';
 // privateKey: 0168474ea1a10e7366cbf6cc96095945d1d833a0c7ed25244f53c24937eda785 for 03c
 
-const privateKey = 'b01f79c616f252130fd877ab692be3ba9432b383eeb30c3c73546581e4ff4aa0';
+const privateKey = '0168474ea1a10e7366cbf6cc96095945d1d833a0c7ed25244f53c24937eda785';
 
 const dataHash = '39156496b0b74926a6b344dc55aff34aba706c96404ae91fafc61314e15c1470';
 
